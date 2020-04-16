7c5864bb7b759d0ea0eeca9e89a60528edf7e804
R Sánchez <rene.as.himself@gmail.com>
2020-04-16 11:37:47 -0500

    fixed styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,25 @@
 <script>
-    import Header from './components/Header'
-    import Footer from '/components/Footer'
-
-
+  import Header from "./components/Header";
+  import Footer from "/components/Footer";
 </script>
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
 
 <Header />
 
 <main>
-    <p>Expedita minima quasi est dolor tempore. Reiciendis asperiores consequatur ratione temporibus et. Ratione quae ut omnis. Officiis occaecati dignissimos vel magnam maxime ut et et. Aliquid perspiciatis dolorem quaerat fugiat nisi. Ut dolor esse.</p>
-
+  <p>
+    Expedita minima quasi est dolor tempore. Reiciendis asperiores consequatur
+    ratione temporibus et. Ratione quae ut omnis. Officiis occaecati dignissimos
+    vel magnam maxime ut et et. Aliquid perspiciatis dolorem quaerat fugiat
+    nisi. Ut dolor esse.
+  </p>
 
 </main>
 
 <Footer />
-
-<style>
-    main {
-        max-width: 960px;
-        margin: 40px auto;
-    }
-</style>
