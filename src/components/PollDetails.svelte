a0964c80ad07b32151c5f43753a63b3381b2e319
R Sánchez <rene.as.himself@gmail.com>
2020-04-28 19:32:31 -0500

    Vote bars

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -7,6 +7,8 @@
   export let poll
 
   $: totalVotes = poll.votesA + poll.votesB
+  $: percentA = Math.floor(100 / totalVotes * poll.votesA)
+  $: percentB = Math.floor(100 / totalVotes * poll.votesB)
 
   const handleVote = (option, id) => {
     dispatch('vote', { option, id })
@@ -20,12 +22,12 @@
     <p>Total votes: {totalVotes}</p>
 
     <div class="answer" on:click={() => handleVote('a', poll.id)} >
-      <div class="percent percent-a"></div>
+      <div class="percent percent-a" style="width: {percentA}%;" ></div>
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
 
     <div class="answer" on:click={() => handleVote('b', poll.id)} >
-      <div class="percent percent-b"></div>
+      <div class="percent percent-b" style="width: {percentB}%;" ></div>
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
   </div>
@@ -58,5 +60,21 @@
   span {
     display: inline-block;
     padding: 10px 20px;
+  }
+
+  .percent {
+    height: 100%;
+    position: absolute;
+    box-sizing: border-box;
+  }
+
+  .percent-a {
+    background: rgba(217,27,66,0.2);
+    border-left: 4px solid #d91b42;
+  }
+
+  .percent-b {
+    background: rgba(69,196,150,0.2);
+    border-left: 4px solid #45c496;
   }
 </style>
